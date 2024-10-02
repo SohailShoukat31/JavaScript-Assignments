8483bdd476df30eb7fa3ccf147998ec425f52489
Sohail Shoukat <sohailshoukat131@gmail.com>
2024-10-02 22:08:46 +0500

    👉Update✨

---
+++ Variables for Strings/app.js
@@ -0,0 +1,59 @@
+// // Declare a variable called username
+var username;
+
+// Declare a variable called myName & assign to it a string  that represents your Full Name.
+var myName = "Sohail Shoukat";
+
+// Declare a JS variable, titled message.
+// Assign “Hello World” to variable message
+// Display the message in alert box.
+var message = "Hello World !";
+alert(message);
+
+// Write a script to save student’s bio data in JS variables and  show the data in alert boxes.
+
+var stdName = "Sohail";
+alert(stdName);
+var stdAge = "21";
+alert(stdAge);
+var jobTitle = "Certifed Mobile Application Development";
+alert(jobTitle);
+
+// //  Write a script to save student’s bio data in JS variables and  show the data in alert boxes
+var stdName = "Sohail";
+var stdAge = 21;
+var jobTitle = "Certified Mobile Application Development";
+document.write(
+  "Name:" +
+    " " +
+    stdName +
+    " " +
+    "\nAge:" +
+    " " +
+    stdAge +
+    " " +
+    "\nJob:" +
+    " " +
+    jobTitle
+);
+
+// let word = "pizaa";
+
+// // Loop through each substring of the word
+// for (let i = word.length; i > 0; i--) {
+//   setTimeout(() => {
+//     Swal.fire(word.substring(0, i)); // Display the substring
+//   }, (word.length - i) * 1000); // Delay each alert by 1 second
+// }
+
+var email = "sohailshoukat149@gmail.com";
+alert("My email address is: " + email);
+
+var book = "A Smarter way to leran Javascript";
+alert("Iam trying to learn Javascript from the book " + book);
+
+var a = "Yah ! I can write HTML content through Javascript";
+document.write(a);
+
+var b = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(b);
